569716b358fca7130e8748aeee2332bb2122adce
mauricio <96304957+MaurisioDTS@users.noreply.github.com>
2025-01-15 20:55:02 +0100

    id cambiados. algunos borrados, otros añadidos

--- Source/Identifiers.h
+++ Source/Identifiers.h
@@ -1,13 +1,3 @@
-/*
-  ==============================================================================
-
-    Identifiers.h
-    Created: 12 Mar 2019 10:34:42am
-    Author:  Joonas
-
-  ==============================================================================
-*/
-
 #pragma once
 
 namespace IDs {
@@ -15,10 +5,8 @@ namespace IDs {
 	#define DECLARE_ID(name) const String name (#name);
 
 	DECLARE_ID(inputVolume)			// Input volume of the effects chain
-	DECLARE_ID(HPFreq)				// Frequency of the highpass filter before the waveshaper
-	DECLARE_ID(LPFreq)				// Frequency of the lowpass filter after the waveshaper	
 	DECLARE_ID(outputVolume)		// Output volume of the effects chain
-	DECLARE_ID(wetDry)				// Wet / dry control of the plugin
+	DECLARE_ID(selector)			// El selector de tipo de distorsión.
 
 	#undef DECLARE_ID
 
